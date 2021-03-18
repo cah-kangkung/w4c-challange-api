2f258ec62bea8a624045db2c3dfc8b55d08bcf6f
cah-kangkung <hafizhun.alim93@gmail.com>
2021-03-18 10:39:33 +0700

    Modify response body

--- application/controllers/api/Rwp.php
+++ application/controllers/api/Rwp.php
@@ -17,48 +17,76 @@ class Rwp extends RestController
       // achievements from json file
       $achievements = json_decode(file_get_contents(FCPATH . 'database/json/achievements.json'));
 
-      $this->response($achievements, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $achievements
+      ], 200);
    }
    public function benefits_get()
    {
       // benefits from json file
       $benefits = json_decode(file_get_contents(FCPATH . 'database/json/benefits.json'));
 
-      $this->response($benefits, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $benefits
+      ], 200);
    }
    public function clients_get()
    {
       // clients from json file
       $clients = json_decode(file_get_contents(FCPATH . 'database/json/clients.json'));
 
-      $this->response($clients, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $clients
+      ], 200);
    }
    public function facilities_get()
    {
       // facilities from json file
       $facilities = json_decode(file_get_contents(FCPATH . 'database/json/facilities.json'));
 
-      $this->response($facilities, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $facilities
+      ], 200);
    }
    public function flows_get()
    {
       // flows from json file
       $flows = json_decode(file_get_contents(FCPATH . 'database/json/flows.json'));
 
-      $this->response($flows, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $flows
+      ], 200);
    }
    public function portfolios_get()
    {
       // portfolios from json file
       $portfolios = json_decode(file_get_contents(FCPATH . 'database/json/portfolios.json'));
 
-      $this->response($portfolios, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $portfolios
+      ], 200);
    }
    public function recommendations_get()
    {
       // recommendations from json file
       $recommendations = json_decode(file_get_contents(FCPATH . 'database/json/recommendations.json'));
 
-      $this->response($recommendations, 200);
+      $this->response($data = [
+         "code" => '200',
+         "response" => 'OK',
+         "data" => $recommendations
+      ], 200);
    }
 }
